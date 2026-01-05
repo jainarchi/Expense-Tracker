08823531b2abfb946c786f69d5fbe2566c6d17a7
jainarchi <imarchijain2405@gmail.com>
2026-01-05 23:40:25 +0530

    commit

---
+++ Frontend/src/components/cards/CharAvatar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CharAvatar = ({ fullName}) => {
+
+  const getInitials = (name) => {
+    if (!name) return "";
+    const words = name.split(" ");
+  
+    const initials = words.map((word) => word[0]).join("");
+    return initials.toUpperCase().substring(0, 2);
+  };
+
+  return (
+    <div
+      className={`flex items-center justify-center rounded-full bg-gray-200 text-gray-600 font-normal uppercase text-2xl h-18 w-18`}
+    >
+      {getInitials(fullName)}
+    </div>
+  );
+};
+
+export default CharAvatar;
--- Frontend/src/utils/helper.js
+++ Frontend/src/utils/helper.js
@@ -11,3 +11,16 @@ export const validatePassword = (password) =>{
 export const validateName = (name) =>{
     return name.length > 3 
 }
+
+export const addThousandsSeparator = (num) =>{
+   if(num == null || isNaN(num)) return ;
+
+   const [integerPart , fractionalPart] = num.toString().split(".");
+   const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g , "," );
+
+
+   return fractionalPart
+   ? `${formattedInteger}.${fractionalPart}`
+   : formattedInteger;
+
+}
